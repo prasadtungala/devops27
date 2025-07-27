d433b65741ee2b35577629cc88155182def5f076
prasaadtungala@gmail.com <prasaadtungala@gmail.com>
2025-07-27 10:08:23 +0530

    Project pushed Ist time

---
+++ WebApp27/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp27.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
